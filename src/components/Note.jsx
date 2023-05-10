7e03c1a9a95c6342a21cec83dc64f4bc223f2ec7
Saijeet Upadhyay <saijeet2019@gmail.com>
2023-05-10 12:41:13 +0545

    Complete : Keeper App Part 1 solution

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>The is the note Title</h1>
+      <p>The is the note content</p>
+    </div>
+  );
+}
+
+export default Note;
