a1820799ac275ff0f128bb49e8f56e29f2b28bb1
Saijeet Upadhyay <saijeet2019@gmail.com>
2023-05-15 17:00:14 +0545

    Complete : create area

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,12 +3,14 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import notes from "../notes";
+import CreateArea from "./CreateArea";
 
 
 function App() {
   return (
     <div>
       <Header />
+      <CreateArea />
       {notes.map((notes) => (
         <Note key={notes.key} title={notes.title} content={notes.content} />
       ))}
